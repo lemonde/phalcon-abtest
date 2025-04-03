08469d513bc5235c993dca892cd566f83fe1e3ba
Nordahl Angot <nangot.pro@gmail.com>
2025-04-03 14:51:54 +0200

    feat(php): bump php to 8.4

--- src/Exception/AbTestingException.php
+++ src/Exception/AbTestingException.php
@@ -4,6 +4,6 @@ namespace ABTesting\Exception;
 
 class AbTestingException extends \Exception
 {
-    public const INCOMPLETE_ANNOTATION = 1;
-    public const UNDEFINED_TEST_CODE = 0;
+    public const int INCOMPLETE_ANNOTATION = 1;
+    public const int UNDEFINED_TEST_CODE = 0;
 }
